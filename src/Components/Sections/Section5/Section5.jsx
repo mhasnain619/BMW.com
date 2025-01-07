c41b77f959359ae85803182f576e830c55b8e52d
Muhammad Hasnain <hasnain0893@gmail.com>
2025-01-08 02:16:25 +0500

    working on the way

--- src/Components/Sections/Section5/Section5.jsx
+++ src/Components/Sections/Section5/Section5.jsx
@@ -8,36 +8,44 @@ import img3 from '../../../assets/skytop.webp'
 
 const Section5 = () => {
     return (
-        <Container fluid>
-            <Row className='sectionRow'>
-                <Col xs={12} md={8} className="TheJoy">
-                    <img src={img1} width={"100%"} />
-                    <div className="overlay-text-left">
-                        <p className="title">Over-the-Air Updates</p>
-                        <p className="subtitle">The Joy of the Ever-Evolving</p>
-                    </div>
-                </Col>
-                <Col className='secTowRow' xs={12} md={4}>
-                    <Row >
-                        <Col className="bmwWallpaper">
-                            <img src={img2} width={"100%"} />
-                            <div className="overlay-text">
-                                <p>BMW wallpapers</p>
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row style={{ borderTop: '1px solid white' }}>
-                        <Col className="skyTop">
-                            <img src={img3} width={"100%"} />
-                            <div className="overlay-text">
-                                <p>BMW Skytop</p>
-                                <p className="subtitle-small">The magic of exclusive moments</p>
-                            </div>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            <div className='allModel'>
+                <h3>ALL BMW MODELS</h3>
+                <p>Find your BMW</p>
+            </div>
+            <Container fluid>
+
+                <Row className='sectionRow'>
+                    <Col xs={12} md={8} className="TheJoy">
+                        <img src={img1} width={"100%"} />
+                        <div className="overlay-text-left">
+                            <p className="title">Over-the-Air Updates</p>
+                            <p className="subtitle">The Joy of the Ever-Evolving</p>
+                        </div>
+                    </Col>
+                    <Col className='secTowRow' xs={12} md={4}>
+                        <Row >
+                            <Col className="bmwWallpaper">
+                                <img src={img2} width={"100%"} />
+                                <div className="overlay-text">
+                                    <p>BMW wallpapers</p>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row style={{ borderTop: '1px solid white' }}>
+                            <Col className="skyTop">
+                                <img src={img3} width={"100%"} />
+                                <div className="overlay-text">
+                                    <p>BMW Skytop</p>
+                                    <p className="subtitle-small">The magic of exclusive moments</p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+
     );
 };
 
