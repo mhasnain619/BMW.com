f6dd7966c16d8d486a8be6aea6f40b4869ab0fe3
Muhammad Hasnain <hasnain0893@gmail.com>
2025-01-07 19:56:32 +0500

    working

--- src/Components/Sections/Section5/Section5.jsx
+++ src/Components/Sections/Section5/Section5.jsx
@@ -1,49 +1,43 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import './Section5.css'
-import CarImg from '../../../assets/BmwAir.webp'
-import wallpaper from '../../../assets/bmdWallpaper.webp'
-import skttop from '../../../assets/skytop.webp'
+import './Section5.css';
+import img1 from '../../../assets/BmwAir.webp'
+
 const Section5 = () => {
     return (
         <Container fluid>
-            <Row >
-                <Col xs={12} md={8} className="TheJoy">
-                    <div className=''>
-                        <p className='text-white'>hello</p>
+            <Row>
+                {/* Left Image Section */}
+                <Col xs={12} md={8} className="TheJoy"
+                    style={{
+                        backgroundImage: `url(${img1})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundRepeat: 'no-repeat',
+                        height: '60vh',
+                    }}>
+                    <div className="overlay-text-left">
+                        <p className="title">Over-the-Air Updates</p>
+                        <p className="subtitle">The Joy of the Ever-Evolving</p>
                     </div>
                 </Col>
+                {/* Right Side Section */}
                 <Col xs={12} md={4}>
+                    {/* Top Section */}
                     <Row className="">
                         <Col className="bmwWallpaper">
-                            <div>
-                                <p
-                                    className="text-center text-white"
-                                    style={{
-                                        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Dark overlay
-                                        padding: '10px',
-                                        borderRadius: '5px',
-                                    }}
-                                >
-                                    BMW Wallpapers
-                                </p>
+                            <div className="overlay-text">
+                                <p>BMW wallpapers</p>
                             </div>
                         </Col>
                     </Row>
+                    {/* Bottom Section */}
                     <Row>
                         <Col className="skyTop">
-                            <div>
-                                <p
-                                    className="text-center text-white"
-                                    style={{
-                                        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Dark overlay
-                                        padding: '10px',
-                                        borderRadius: '5px',
-                                    }}
-                                >
-                                    BMW Skytop
-                                </p>
+                            <div className="overlay-text">
+                                <p>BMW Skytop</p>
+                                <p className="subtitle-small">The magic of exclusive moments</p>
                             </div>
                         </Col>
                     </Row>
